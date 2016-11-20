6040d00d6c36dcd9ed6e11725c22fcf9bcfb0be2
sunand-472 <sunand.matam@gmail.com>
2016-11-20 18:08:31 -0500

    added println statement

--- GitSample/src/HelloWorld.java
+++ GitSample/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	    System.out.println("This  is the first program");
+	    System.out.println("Welcome to git integration with the eclipse");
 	    
 	}
 	
